5a64ddd7216b4f322d888bda5482bc7ca30c3d55
Aydin <aydin@wearejh.com>
2014-01-28 14:33:45 +0000

    PHPUnit config

---
+++ htdocs/phpunit-bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Include composer autoloader for PHPUnit dependencies
+ *
+ * @author Aydin Hassan <aydin@wearejh.com>
+ */
+
+//Include composer's autoloader
+include "../vendor/autoload.php";
+
+//Include EcomDev unit's bootstrap
+include "app/code/community/EcomDev/PHPUnit/bootstrap.php";
